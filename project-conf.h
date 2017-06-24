3777d3f6b9b2ef6f0fcfe7af92fbd85b85527dec
Nevin Allwood <nevinallwood@gmail.com>
2017-06-24 10:57:18 +0200

    first

---
+++ project-conf.h
@@ -0,0 +1,13 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#undef NETSTACK_RDC_CHANNEL_CHECKRATE
+#define NETSTACK_RDC_CHANNEL_CHECKRATE      16
+
+#undef RF_CHANNEL
+#define RF_CHANNEL                          26
+
+#undef CC2420_CONF_CHANNEL
+#define CC2420_CONF_CHANNEL                 26
+
+#endif
---
+++ sender.c
@@ -0,0 +1,85 @@
+#include "contiki.h"
+#include "lib/random.h"
+#include "sys/ctimer.h"
+#include "sys/node-id.h"
+
+#include "net/ip/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ip/uip-udp-packet.h"
+#include "net/ipv6/uip-ds6-route.h"
+
+#include <stdio.h>
+
+#include "dev/leds.h"
+/*---------------------------------------------------------------------------*/
+#define UDP_SENDER_PORT 8765
+#define UDP_RECEIVER_PORT 5678
+#define SEND_INTERVAL		0.5
+
+typedef struct msg{
+  uint16_t nodeId;
+} msg_t;
+/*---------------------------------------------------------------------------*/
+static struct uip_udp_conn *sender_conn;
+static uip_ipaddr_t receiver_ipaddr;
+static struct etimer periodic;
+static msg_t data;
+/*---------------------------------------------------------------------------*/
+PROCESS(sender_process, "sender process");
+AUTOSTART_PROCESSES(&sender_process);
+/*---------------------------------------------------------------------------*/
+static void collectData(){
+  data.nodeId = node_id;
+}
+/*---------------------------------------------------------------------------*/
+static void sendData(){
+  printf("sending data\n");
+  collectData();
+  uip_udp_packet_sendto(sender_conn, &data, sizeof(data),
+                        &receiver_ipaddr, UIP_HTONS(UDP_RECEIVER_PORT));
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(sender_process, ev, data){
+  PROCESS_BEGIN();
+  uip_ipaddr_t ipaddr;
+
+  leds_on(LEDS_GREEN);
+  etimer_set(&periodic, CLOCK_SECOND*5);
+  PROCESS_WAIT_EVENT();
+
+  NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER,31);
+  NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,26);
+
+  /* set global adress */
+  uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+  uip_ip6addr(&receiver_ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 1);
+
+  /* setup udp connection */
+  sender_conn = udp_new(NULL, UIP_HTONS(UDP_RECEIVER_PORT), NULL);
+  if(sender_conn == NULL) {
+    printf("No UDP connection available, exiting the process!\n");
+    PROCESS_EXIT();
+  }
+  udp_bind(sender_conn, UIP_HTONS(UDP_SENDER_PORT));
+
+  printf("Created a connection with the receiver\n");
+
+/* send sensor data periodically */
+  etimer_set(&periodic, SEND_INTERVAL * CLOCK_SECOND);
+  while(1) {
+    leds_toggle(LEDS_RED);
+    PROCESS_WAIT_EVENT();
+
+    if(etimer_expired(&periodic)) {
+      sendData();
+      etimer_reset(&periodic);
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
